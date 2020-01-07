ba77dd20746da9400661235733f38b1f2a8f7a77
mamatb <mamatb@localhost.localdomain>
2020-01-07 16:25:05 +0100

    Initial version

---
+++ EAP_buster.sh
@@ -0,0 +1,238 @@
+#!/usr/bin/env bash
+
+# EAP_buster by BlackArrow [https://github.com/blackarrowsec/EAP_buster]
+#
+# Description
+# EAP_buster is a simple bash script that lists what EAP methods are supported
+# by the RADIUS server behind a WPA-Enterprise access point. In order to achieve
+# this, it makes use of several wpa_supplicant configuration files along with
+# WPA-Enterprise identities, which can be grabbed with some passive sniffing.
+#
+# Author
+# Miguel Amat (t.me/m_amatb) from BlackArrow
+#
+# Details
+# https://github.com/blackarrowsec/EAP_buster/README.md
+#
+# Web
+# [www.blackarrow.net] - [www.tarlogic.com]
+
+# TODO
+#
+# - Parse arguments using getops instead of manually
+# - Restore wpa_supplicant original status after execution
+# - Be able to grab various EAP identities from a file to avoid network bans
+# - Thoroughly test the existent configuration files
+# - Find pending configuration files for weird EAP methods
+# - Detect network bans and set incremental timers
+# - Group tries by phase1 methods to discard all not supported phase2 variants
+
+EAP_method_list=(
+'EAP-TLS'
+'EAP-PEAP_MSCHAPV2'
+'EAP-PEAP_TLS'
+'EAP-PEAP_GTC'
+'EAP-PEAP_OTP'
+'EAP-PEAP_MD5-Challenge'
+'EAP-TTLS_EAP-MD5-Challenge'
+'EAP-TTLS_EAP-GTC'
+'EAP-TTLS_EAP-OTP'
+'EAP-TTLS_EAP-MSCHAPV2'
+'EAP-TTLS_EAP-TLS'
+'EAP-TTLS_MSCHAPV2'
+'EAP-TTLS_MSCHAP'
+'EAP-TTLS_PAP'
+'EAP-TTLS_CHAP'
+'EAP-SIM'
+'EAP_AKA'
+'EAP-PSK'
+'EAP-PAX'
+'EAP-SAKE'
+'EAP-IKEv2'
+'EAP-GPSK'
+'LEAP'
+'EAP-FAST_MSCHAPV2'
+'EAP-FAST_GTC'
+'EAP-FAST_OTP'
+)
+
+# attributes needed to build wpa_supplicant configuration files
+EAP_attributes=(
+'ssid'
+'identity'
+'client_cert'
+'private_key'
+'key_passwd'
+'client_cert2'
+'private_key2'
+'key2_passwd'
+)
+
+# colored output for EAP methods that are supported
+print_supported()
+{
+	echo -e "\r\033[K\033[0;32msupported\033[0m      =>  ${1}"
+}
+
+# colored output for EAP methods that are not supported
+print_not_supported()
+{
+	echo -e "\r\033[K\033[0;31mnot supported\033[0m  =>  ${1}"
+}
+
+# BlackArrow banner
+print_banner()
+{
+	echo -e "\nEAP_buster by BlackArrow [https://github.com/blackarrowsec/EAP_buster]\n"
+}
+
+print_banner
+
+# warning about using legitimate EAP identities
+print_identities_warning()
+{
+	echo -e '\033[1;33mWARNING\033[0m\nYou need to use legitimate EAP identities in order to start the 802.1X authentication process and get reliable results (if Protected Management Frames are not in use, EAP identites can be collected using sniffing tools such as crEAP, just make sure you use a real identity and not an anonymous one => https://github.com/Snizz/crEAP)\n'
+}
+
+print_identities_warning
+
+# checking user permissions
+if [ "${USER}" != 'root' ]
+then
+	echo -e "\033[0;31mPERMISSIONS ERROR\033[0m\nYou need to be root to run ${0}, wpa_supplicant has to be started and stopped several times during execution\n"
+	exit 1
+fi
+
+# checking number of arguments
+if [ "${#}" != '3' ]
+then
+	echo -e "\033[0;31mSYNTAX ERROR\033[0m\n${0} <EAP_SSID> <EAP_identity> <wireless_interface>\n"
+	exit 1
+fi
+
+EAP_SSID="${1}"
+EAP_identity="${2}"
+wireless_interface="${3}"
+EAP_buster_dir="$(readlink -f $(dirname "${0}"))"
+EAP_config_dir="${EAP_buster_dir}/EAP_config"
+EAP_log_dir="${EAP_buster_dir}/${EAP_SSID}"
+
+# checking wireless_interface existence and saving original mac address
+if ! iw "${wireless_interface}" 'info' &> '/dev/null'
+then
+	echo -e "\033[0;31mINPUT ERROR\033[0m\n3rd argument "'"'"${wireless_interface}"'"'" is not a valid wireless interface\n"
+	exit 1
+else
+	wireless_interface_mac="$(iw "${wireless_interface}" 'info' | grep 'addr' | cut --delimiter=' ' --fields='2')"
+fi
+
+# checking EAP_buster_dir permissions
+if [ ! -r "${EAP_buster_dir}" ] || [ ! -w "${EAP_buster_dir}" ]
+then
+	echo -e "\033[0;31mPERMISSIONS ERROR\033[0m\nYou need read and write permissions in ${EAP_buster_dir}\n"
+	exit 1
+fi
+
+# checking EAP_config_dir permissions
+if [ ! -r "${EAP_config_dir}" ]
+then
+	echo -e "\033[0;31mPERMISSIONS ERROR\033[0m\nYou need read permissions in ${EAP_config_dir} to access configuration files\n"
+	exit 1
+fi
+
+# checking EAP_log_dir permissions (and creation if needed)
+if [ ! -d "${EAP_log_dir}" ]
+then
+	mkdir "${EAP_log_dir}"
+elif [ ! -r "${EAP_log_dir}" ] || [ ! -w "${EAP_log_dir}" ]
+then
+	echo -e "\033[0;31mPERMISSIONS ERROR\033[0m\nYou need read and write permissions in ${EAP_log_dir} to create and access wpa_supplicant logs\n"
+	exit 1
+fi
+
+# change to random or restore mac address to avoid network bans
+modify_mac_address()
+{
+	ifconfig "${wireless_interface}" 'down'
+	if [ "${1}" == 'change' ]
+	then
+		urandom_6="$(xxd -plain -len '6' '/dev/urandom')"
+		wireless_interface_mac_new="${urandom_6:0:1}0:${urandom_6:2:2}:${urandom_6:4:2}:${urandom_6:6:2}:${urandom_6:8:2}:${urandom_6:10:2}"
+		ifconfig "${wireless_interface}" 'hw' 'ether' "${wireless_interface_mac_new}"
+	elif [ "${1}" == 'restore' ]
+	then
+		ifconfig "${wireless_interface}" 'hw' 'ether' "${wireless_interface_mac}"
+	fi
+	ifconfig "${wireless_interface}" 'up'
+}
+
+# values needed to build wpa_supplicant configuration files
+EAP_values=(
+"${EAP_SSID}"
+"${EAP_identity}"
+"${EAP_buster_dir}/user.pem"
+"${EAP_buster_dir}/user.key"
+'whatever'
+"${EAP_buster_dir}/user.pem"
+"${EAP_buster_dir}/user.key"
+'whatever'
+)
+
+# network interface mode configuration
+ifconfig "${wireless_interface}" 'down'
+iwconfig "${wireless_interface}" 'mode' 'managed'
+ifconfig "${wireless_interface}" 'up'
+
+# certificate + key generation using the specified identity and SSID
+openssl 'req' -x509 -newkey 'rsa:4096' -keyout "${EAP_buster_dir}/user.key" -out "${EAP_buster_dir}/user.pem" -days '365' -passout 'pass:whatever' -subj "/CN=${EAP_identity}/O=${EAP_SSID}" &> '/dev/null'
+
+# stop wpa_supplicant before starting
+killall --quiet 'wpa_supplicant'
+
+# main loop between EAP methods
+for EAP_method in "${EAP_method_list[@]}"
+do
+	EAP_config_file="${EAP_config_dir}/${EAP_method}.conf"
+	if [ -f "${EAP_config_file}" ] && [ -r "${EAP_config_file}" ] && [ -w "${EAP_config_file}" ]
+	then
+		EAP_log_file="${EAP_log_dir}/${EAP_SSID}_${EAP_method}.log"
+		echo '' > "${EAP_log_file}"
+		echo -n "checking ${EAP_method} support ..."
+		
+		# wpa_supplicant attributes configuration
+		for EAP_tuple in $(seq '0' "$(("${#EAP_attributes[@]}" - "1"))")
+		do
+			sed --in-place "s|${EAP_attributes[${EAP_tuple}]}=.*|${EAP_attributes[${EAP_tuple}]}=\"${EAP_values[${EAP_tuple}]}\"|g" "${EAP_config_file}"
+		done
+		
+		# mac address change and wpa_supplicant execution
+		modify_mac_address 'change'
+		timeout '10' wpa_supplicant -d -K -D 'nl80211' -i "${wireless_interface}" -c "${EAP_config_file}" -f "${EAP_log_file}"
+		sleep '5'
+		
+		# check log file to identify supported EAP methods
+		if grep --quiet 'EAP: Status notification: accept proposed method' "${EAP_log_file}"
+		then
+			if grep --quiet 'TLS: Phase 2 Request: Nak' "${EAP_log_file}"
+			then
+				if grep --quiet 'Selected Phase 2' "${EAP_log_file}"
+				then
+					print_supported "${EAP_method}"
+				else
+					print_not_supported "${EAP_method}"
+				fi
+			
+			# no 'Phase 2 Nak' at this point means that either the phase 2 has been selected or there is no phase 2 at all
+			else
+				print_supported "${EAP_method}"
+			fi
+		else
+			print_not_supported "${EAP_method}"
+		fi
+	fi
+done
+
+echo ''
+modify_mac_address 'restore'
+
+exit 0
---
+++ README.md
@@ -0,0 +1,55 @@
+# EAP_buster
+ [![](https://img.shields.io/badge/Category-Recon-E5A505?style=flat-square)]() [![](https://img.shields.io/badge/Language-Bash-E5A505?style=flat-square)]()
+
+
+<br>
+EAP_buster is a simple bash script that lists what EAP methods are supported by the RADIUS server behind a WPA-Enterprise access point. In order to achieve this, it makes use of several wpa_supplicant configuration files along with WPA-Enterprise identities, which can be grabbed with some passive sniffing.
+
+Offensive usage
+---------------
+
+During Red Team exercises, EAP_buster provides hints about what to expect from WiFi clients when launching fake WPA-Enterprise access points.
+
+Example: forget about grabbing passwords with fake AP attacks if clients are expected to authenticate through certificates only (EAP-TLS). Usage:
+
+	./EAP_buster.sh <EAP_SSID> <EAP_identity> <wireless_interface>
+
+![image](offensive_usage.png)
+
+It should be noted that EAP_buster needs legitimate identities in order to start the 802.1X authentication process and get reliable results.
+
+If Protected Management Frames are not in use, EAP identites can be passively collected using sniffing tools such as [crEAP](https://github.com/Snizz/crEAP), just make sure you use a real identity and not an anonymous one.
+
+Defensive usage
+---------------
+
+From a Systems Administrator standpoint, EAP_buster can be used to detect fake WPA-Enterprise access points, as they tend to support as many EAP methods as possible in order to offer legitimate clients every form of authentication (even if using clearly fake identities).
+
+The following image shows what EAP methods are supported by default when using `hostapd-wpe`:
+
+![image](defensive_usage.png)
+
+Installation
+------------
+
+No installation process needed, EAP_buster's main functionality consists on launching wpa_supplicant several times.
+
+Author and Credits
+---------------
+Author: Miguel Amat [@mamatb](https://t.me/m_amatb)
+
+References:
+
+* [EAP packet types](https://github.com/secdev/scapy/blob/master/scapy/layers/eap.py)
+* [Typical 802.1X authentication](https://en.wikipedia.org/wiki/IEEE_802.1X#Typical_authentication_progression)
+* [wpa_supplicant description](https://github.com/digsrc/wpa_supplicant/blob/master/wpa_supplicant/README)
+* [/usr/share/doc/wpasupplicant/examples/wpa_supplicant.conf](https://w1.fi/cgit/hostap/plain/wpa_supplicant/wpa_supplicant.conf)
+
+License
+-------
+
+All the code included in this project is licensed under the terms of the MIT license.
+
+#
+
+[![](https://img.shields.io/badge/www-blackarrow.net-E5A505?style=flat-square)](https://www.blackarrow.net) [![](https://img.shields.io/badge/twitter-@BlackArrowSec-00aced?style=flat-square&logo=twitter&logoColor=white)](https://twitter.com/BlackArrowSec) [![](https://img.shields.io/badge/linkedin-@BlackArrowSec-0084b4?style=flat-square&logo=linkedin&logoColor=white)](https://www.linkedin.com/company/blackarrowsec/)
